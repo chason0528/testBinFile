0ead563172e2e26dc655f38ec161b5fe8b4a4891
x <x@xs-MacBook-Pro.local>
2020-01-01 23:54:48 +0800

    delete obj files

--- android/obj/local/arm64-v8a/objs/test/test.o.d
+++
@@ -1,2 +0,0 @@
-/Users/x/PycharmProjects/untitled/testbinFile/android/obj/local/arm64-v8a/objs/test/test.o: \
-  /Users/x/PycharmProjects/untitled/testbinFile/android/jni/test.c
--- android/obj/local/x86_64/objs/test/test.o.d
+++
@@ -1,2 +0,0 @@
-/Users/x/PycharmProjects/untitled/testbinFile/android/obj/local/x86_64/objs/test/test.o: \
-  /Users/x/PycharmProjects/untitled/testbinFile/android/jni/test.c
