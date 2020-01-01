928b89f8dd107016621b36995c36c8ac2dcd8ceb
x <x@xs-MacBook-Pro.local>
2020-01-01 23:43:53 +0800

    added android arch test file

---
+++ android/obj/local/armeabi-v7a/objs/test/test.o.d
@@ -0,0 +1,2 @@
+/Users/x/PycharmProjects/untitled/testbinFile/android/obj/local/armeabi-v7a/objs/test/test.o: \
+  /Users/x/PycharmProjects/untitled/testbinFile/android/jni/test.c
---
+++ android/obj/local/x86_64/objs/test/test.o.d
@@ -0,0 +1,2 @@
+/Users/x/PycharmProjects/untitled/testbinFile/android/obj/local/x86_64/objs/test/test.o: \
+  /Users/x/PycharmProjects/untitled/testbinFile/android/jni/test.c
